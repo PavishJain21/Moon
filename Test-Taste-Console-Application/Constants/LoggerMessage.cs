80e75d47ec15a192dea231361a7c2e9da0915e48
PavishJain21 <jainpavish@gmail.com>
2023-09-03 13:00:02 +0530

    initial commit

---
+++ Test-Taste-Console-Application/Constants/LoggerMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Test_Taste_Console_Application.Constants
+{
+    public static class LoggerMessage
+    {
+        public const string GetRequestFailed = "Get request failed with the code: ";
+        public const string ScreenOutputOperationFailed =
+            "The screen output operation failed with the following error: ";
+        public const string FileOutputOperationFailed = "The file output operation failed with the following error: ";
+        public const string LoadingPlanets = "------------------Loading Planets and their Average Moon Gravity -----------------";
+        public const string LoadedSuccessfully = "------------------ Loaded Successfully -----------------------------------------";
+        public const string GetsMoonAndMass = "------------------Getting Moons and their Mass -----------------------------------";
+        public const string PlanetAndTheirMoons = "------------------Getting Planets and their Moons --------------------------------";
+        public const string WritingData = "WritingData";
+    }
+}
---
+++ Test-Taste-Console-Application/Excercise_2/Exercise.cs
@@ -0,0 +1,11 @@
+﻿//excersise-1
+// gravity mapping was missing so i have adeded it to MoonDTO
+// and moon and calculated average and created constant for having logging in console.
+
+
+/// one drawback is as its taking too long to display data instead
+// we can fetch planets and then display their gravity value one by one instead fetching all at one
+
+
+// We need to collect all the information required and we can run all the three api's
+// parallely that would be helpful in the optimization
